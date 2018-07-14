0cae32bf89ae10bf2f98386822cfda19881e9402
Aswin Karuvally <aswinbabuk@gmail.com>
2018-07-15 00:29:27 +0530

    working file to array

--- quark/file_convert.c
+++ quark/file_convert.c
@@ -1,36 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdint.h>
+
+typedef uint8_t u8;
 
 // the main function
 int main(int argc, char* argv[])
 {
   // essential variables
   long file_size;
+  int i;
 
   // read the file
   if(argc > 1)
   {
+    // create the file pointer
     FILE *file_pointer;
     file_pointer = fopen(argv[1], "rb");
 
+    // find the file length
     fseek(file_pointer, 0, SEEK_END);
     file_size = ftell(file_pointer);
     fseek(file_pointer, 0, SEEK_SET);
 
-
+    // read the file contents
     char *file_contents = malloc(file_size + 1);
     fread(file_contents, file_size, 1, file_pointer);
 
-    printf("%c", file_contents);
-
-
-    /*
-    // convert to hex
-    for(i=0; i<file_size; ++i)
+    // convert contents to integer array
+    u8 *digest_input = malloc(file_size + 1);
+    for(i = 0; i < file_size; ++i)
     {
-      
+      digest_input = file_contents;
+      ++file_contents;
+      ++digest_input;
     }
-    */
   }
 
   return 0;
--- quark/main.c
+++ quark/main.c
@@ -36,24 +36,36 @@ int test_vectors()
 }
 
 
-// the main function
 int main(int argc, char* argv[])
 {
   // essential variables
   long file_size;
+  int i;
 
   // read the file
   if(argc > 1)
   {
+    // create the file pointer
     FILE *file_pointer;
     file_pointer = fopen(argv[1], "rb");
 
+    // find the file length
     fseek(file_pointer, 0, SEEK_END);
     file_size = ftell(file_pointer);
     fseek(file_pointer, 0, SEEK_SET);
 
+    // read the file contents
     char *file_contents = malloc(file_size + 1);
     fread(file_contents, file_size, 1, file_pointer);
+
+    // convert contents to integer array
+    u8 *digest_input = malloc(file_size + 1);
+    for(i = 0; i < file_size; ++i)
+    {
+      digest_input = file_contents;
+      ++file_contents;
+      ++digest_input;
+    }
   }
 
   return 0;
