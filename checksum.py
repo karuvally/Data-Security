7821c951b486e015895de2b4c1b47abad624c5f4
Aswin Karuvally <aswinbabuk@gmail.com>
2018-07-15 11:57:28 +0530

    ability to create database

--- checksum.py
+++ checksum.py
@@ -6,6 +6,8 @@
 import zlib
 import argparse
 import sys
+import sqlite3
+import os
 
 
 # read and return list of files
@@ -36,12 +38,29 @@ def generate_checksum(list_of_files):
             print("error: unable to read", file_path)
             sys.exit(1)
 
+    return checksums
+
+
+# setup the sqlite database
+def setup_database(database_path):
+    connection = sqlite3.connect(database_path)
+    
+    connection.executescript("""
+    create table file_checksum(
+        id integer primary key autoincrement not null,
+        file_path text unique,
+        checksum text not null
+    """)
+
+    connection.close()
+
 
 # the main function
 def main():
     # read the run-time arguments
     parser = argparse.ArgumentParser(description =
-        "Looks for changes in files")
+    "Looks for changes in files")
+    
     parser.add_argument("file_list", help = "specify path to file list")
     arguments = parser.parse_args()
 
@@ -52,6 +71,10 @@ def main():
     checksums = generate_checksum(list_of_files)
 
     # check if file already has hash in database
+    database_path = os.path.join("home", os.getlogin(), ".file_checksum.db")
+    if not os.path.exists(database_path):
+        setup_database(database_path)
+
     # if not, create the entry in db
     # cross check both hashes
     # alert user if change in hash
